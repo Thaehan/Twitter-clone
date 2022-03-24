39c1b302c4152c3fa2a8eaf61a7e29e56272f4c2
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2022-03-24 18:38:15 +0700

    message so sai

---
+++ src/components/ChatBubble.js
@@ -0,0 +1,76 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+function Bubble_Send({ content }) {
+  return (
+    <TouchableOpacity
+      style={message_send.bubble}
+
+      //onPress={onPress}
+    >
+      <Text style={message_send.text_message}>
+        {content}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+function Bubble_Received({ content }) {
+  return (
+    <TouchableOpacity
+      style={message_received.bubble}
+      //onPress={onPress}
+    >
+      <Text style={message_received.text_message}>
+        {content}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const message_send = StyleSheet.create({
+  bubble: {
+    alignSelf: 'flex-end',
+    textAlign: 'right',
+    alignContent: 'center',
+    marginRight: 40,
+    marginTop: 5,
+    fontWeight: '500',
+    borderRadius: 100,
+    backgroundColor: '#1DA1F2',
+  },
+  text_message: {
+    fontWeight: '500',
+    marginTop: 6,
+    marginBottom: 8,
+    marginLeft: 15,
+    marginRight: 15,
+    color: '#ffffff',
+  },
+});
+
+const message_received = StyleSheet.create({
+  bubble: {
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    alignContent: 'left',
+    marginLeft: 40,
+    marginTop: 5,
+    fontWeight: '500',
+    borderRadius: 100,
+    backgroundColor: '#EFF3F4',
+  },
+  text_message: {
+    fontWeight: '500',
+    marginTop: 6,
+    marginBottom: 8,
+    marginLeft: 15,
+    marginRight: 15,
+    color: '##5C6165',
+  },
+});
+
+export { Bubble_Send, Bubble_Received };
--- src/screens/MainScreen/Message.js
+++ src/screens/MainScreen/Message.js
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-
+import { Bubble_Send, Bubble_Received } from '../../components/ChatBubble';
 import { GLOBAL_STYLES } from '../../styles/Style';
 
 export default function Message() {
   return (
     <View style={GLOBAL_STYLES.container}>
       <Text>Message</Text>
+      <Bubble_Send content='tin nhan da gui 1'/>
+      <Bubble_Send content='tin nhan da gui 2'/>
+      <Bubble_Received content='tin nhan da nhan 1'/>
+      <Bubble_Received content='tin nhan da nhan 2'/>
+      <Bubble_Received content='tin nhan da nhan dai dai dai dai dai dai vai vai vai vai'/>
     </View>
   );
 }
