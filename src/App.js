6cdb6e5db0ec4f6609619ed7d8ba7c17ba1712ef
Thaehan <19021236@vnu.edu.vn>
2022-03-30 23:30:59 +0700

    add react-native-elements packages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { View } from 'react-native';
 
 import RootNavigator from './navigation/RootNavigator';
 //Nhận vào props: avatar, avatarPress, onPress, fullname, username, content
-import Comment from './components/tweet/Comment';
-import IconButton from './components/button/IconButton';
 
 export default function App() {
   return <RootNavigator />;
